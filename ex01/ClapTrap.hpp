49c317ab5276c03498064ed987b7c302291119b3
nicolasgsantana <nicolas07san@gmail.com>
2026-08-31 10:27:11 -0300

    add initial files

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string		m_name;
+	unsigned int	m_hitPoints;
+	unsigned int	m_energyPoints;
+	unsigned int	m_attackDamage;
+
+public:
+	ClapTrap(void);
+	ClapTrap(const ClapTrap &obj);
+	ClapTrap &operator=(const ClapTrap &obj);
+	~ClapTrap(void);
+
+	ClapTrap(const std::string &name);
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
